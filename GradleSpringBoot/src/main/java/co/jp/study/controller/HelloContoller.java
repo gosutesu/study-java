e1459ab45a0aa776ab9665205debdd84e264d5d6
gosutesu <gosutesu@gmail.com>
2019-08-06 21:05:57 +0900

    コメントを最新化

--- GradleSpringBoot/src/main/java/co/jp/study/controller/HelloContoller.java
+++ GradleSpringBoot/src/main/java/co/jp/study/controller/HelloContoller.java
@@ -3,6 +3,9 @@ package co.jp.study.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * サンプルRestControllerクラス
+ */
 @RestController("/")
 public class HelloContoller {
 
